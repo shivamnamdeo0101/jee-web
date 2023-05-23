6e20808e53edcdb5bb1c399b189ca986a35b1d07
shinamde <Wsxokn@#14>
2023-05-23 21:01:40 +0530

    meta added

---
+++ src/BannerComp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function BannerComp() {
+  return (
+    <div className='bannner'>
+        <h1>GET GUIDANCE</h1>
+        <h1>Get Expert Guidance Till Your College Admission</h1>
+    </div>
+  )
+}
+
+export default BannerComp
